8b39ecf22f6a2ddb170648eb010de30802794d64
dalto <dalton.m.towe@gmail.com>
2023-02-04 21:24:38 -0500

    yearly maintenance cost

--- src/YearlyMaintenance.java
+++ src/YearlyMaintenance.java
@@ -3,6 +3,16 @@ public class YearlyMaintenance
     public static void main(String[] args)
     {
 
-        
+        double springMaintenance = 500;
+        double summerMaintenance = 200;
+        double fallMaintenance = 400;
+        double winterMaintenance = 600;
+        double totalMaintenance;
+
+        totalMaintenance = springMaintenance + summerMaintenance + fallMaintenance + winterMaintenance;
+
+        System.out.println("Your spring maintenance cost is " + springMaintenance + "Your summer maintenance cost is " + summerMaintenance + "Your fall maintenance cost is " + fallMaintenance + "Your winter maintenance cost is " + winterMaintenance);
+        System.out.println("Your total yearly maintenance cost is " + totalMaintenance);
+
     }
 }
